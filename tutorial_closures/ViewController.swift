d53c06eb9798d4d5cb2639308fc9a3464628fe5c
Dava <david.martinez@unseen.is>
2017-05-06 14:53:22 -0400

    initial commit

---
+++ tutorial_closures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tutorial_closures
+//
+//  Created by Dava on 5/6/17.
+//  Copyright © 2017 Davaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
