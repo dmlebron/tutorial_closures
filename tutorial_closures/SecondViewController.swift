7ba1f7143e98e16ae776a27a6e46ff565df6fb09
Dava <david.martinez@unseen.is>
2017-05-13 16:29:59 -0400

    tutorial finished

--- tutorial_closures/SecondViewController.swift
+++ tutorial_closures/SecondViewController.swift
@@ -9,27 +9,122 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    fileprivate lazy var textField: UITextField = {
+       
+        let txt = UITextField()
+        txt.returnKeyType = .done
+        txt.backgroundColor = .groupTableViewBackground
+        txt.layer.cornerRadius = 3.0
+        
+        
+        txt.translatesAutoresizingMaskIntoConstraints = false
+        txt.delegate = self
+        
+        return txt
+    }()
+    
+    fileprivate lazy var cancelButton: UIButton = {
+        
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Cancel", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    fileprivate lazy var updateButton: UIButton = {
+        
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Update", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(update), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    public var buttonAction: ((String?) -> ())?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.addSubview(textField)
+        view.addSubview(cancelButton)
+        view.addSubview(updateButton)
+        
+        addConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        textField.becomeFirstResponder()
     }
-    */
-
 }
+
+//MARK: UITextField Delegate
+extension SecondViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        hideKeyboard()
+        
+        return false
+    }
+    
+}
+
+//MARK: Private Methods
+private extension SecondViewController {
+    func addConstraints() {
+        
+        // textfield
+        textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
+        textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        
+        // cancel button
+        cancelButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        cancelButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        cancelButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 20).isActive = true
+        cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        // update button
+        updateButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        updateButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        updateButton.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 20).isActive = true
+        updateButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        view.setNeedsLayout()
+    }
+    
+    /// Resigns First Responder
+    func hideKeyboard() {
+        textField.resignFirstResponder()
+    }
+    
+    @objc func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func update() {
+        guard let buttonAction = buttonAction else {
+            return dismiss(animated: true, completion: nil)
+        }
+        
+        buttonAction(textField.text)
+        
+    }
+}
+
+
+
+
+
+
+
--- tutorial_closures/ViewController.swift
+++ tutorial_closures/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    fileprivate var textLabel: UILabel = {
+        
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .black
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.text = "My Label"
+        
+        return label
+    }()
+    
+    fileprivate lazy var showVCButton: UIButton = {
+        
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Show", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 8.0
+        button.backgroundColor = .black
+        button.addTarget(self, action: #selector(showViewController(sender:)), for: .touchUpInside)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(textLabel)
+        view.addSubview(showVCButton)
+        
+        addConstraints()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc internal func showViewController(sender: UIButton) {
+        
+        if let secondViewController = storyboard?.instantiateViewController(withIdentifier: "secondViewController") as? SecondViewController {
+            
+            secondViewController.buttonAction = {(text) -> () in
+                self.textLabel.text = text
+                return secondViewController.dismiss(animated: true, completion: nil)
+            }
+            
+            present(secondViewController, animated: true, completion: nil)
+        }
+        
     }
-
-
+    
+    private func addConstraints() {
+        // label constraints
+        textLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8).isActive = true
+        textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8).isActive = true
+        textLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        // button contraints
+        showVCButton.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 16).isActive = true
+        showVCButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        showVCButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        showVCButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        view.setNeedsLayout()
+    }
 }
 
